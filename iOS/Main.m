b24e1bcb024fff0dec2dcb570539ac2311eb8370
ILovePuppies9145 <houston.cook.a@gmail.com>
2025-11-17 21:57:13 -0700

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://147.185.221.31:11547/"
+#define API_URL @"https://api-v1-platform-service-prod.erafn.dev/"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
